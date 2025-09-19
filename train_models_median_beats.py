cb1992e4b518a2b1b757c651e5d0fb91859a353a
egeozkoc <egeozkoc@gmail.com>
2025-09-19 14:35:31 -0400

    Revert "some more changes recommended by gpt5"
    This reverts commit 31b803d2146f51fac9b7f5237f433ed762f157e8.

--- train_models_median_beats.py
+++ train_models_median_beats.py
@@ -88,7 +88,7 @@ def val_epoch(model, device, val_dataloader, criterion):
             y = y.to(device)
             x = torch.unsqueeze(x, 1)
 
-            with torch.amp.autocast(device.type, enabled=(device.type == 'cuda')):
+            with torch.amp.autocast(device.type, enabled=True):
                 y_pred = model(x)
                 loss = criterion(y_pred, y)
                 y_pred = torch.softmax(y_pred, dim=-1)
@@ -224,10 +224,8 @@ if __name__ == '__main__':
         
         scaler = torch.amp.GradScaler(device=device, enabled=True)
 
-        train_loader = DataLoader(train_dataset, batch_size=bs, shuffle=True,
-                          num_workers=4, pin_memory=(device.type=='cuda'), persistent_workers=True)
-        val_loader   = DataLoader(val_dataset,   batch_size=bs, shuffle=False,
-                          num_workers=4, pin_memory=(device.type=='cuda'), persistent_workers=True)
+        train_loader = DataLoader(train_dataset, batch_size=bs, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=bs, shuffle=False)
 
         best_val_loss = np.inf
         count = 0
